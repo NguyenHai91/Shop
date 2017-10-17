43312e3923508130e19a9fe1bc25807ea46a2e44
Nguyen Hai <hainguyen4391@gmail.com>
2017-10-17 13:13:33 +0700

    @ changed

--- js/appCart.js
+++ js/appCart.js
@@ -1,5 +1,34 @@
 window.addEventListener('DOMContentLoaded', function(events)
 {
+	let body = document.getElementsByTagName("body")[0];
+
+	let loginBox = document.getElementById("login-register");
+	// show login and register box
+	let account = document.getElementById("account");
+	account.addEventListener("click", function(event)
+	{
+		var display = loginBox.style.display;
+		loginBox.style.display = 'block';
+	});
+	// close login and register box
+	let closeLogin = document.getElementById('close-login-box');
+	closeLogin.addEventListener("click", function(event)
+	{
+		loginBox.style.display = 'none';
+	});
+	// toggle mobile menu
+	let btnMenu = document.getElementById("btn-menu");
+	let mobileMenu = document.getElementById("mobile-menu");
+	btnMenu.addEventListener("click", function(event)
+	{
+		let isShow = mobileMenu.style.display;
+		if (isShow == "none") {
+			mobileMenu.style.display = "block";
+		} else{
+			mobileMenu.style.display = "none";
+		};
+		
+	});
 	let quantity = document.getElementsByClassName("quantity-btn");
 	let btnSub = [];
 	let btnPlus = [];
--- js/appDetail.js
+++ js/appDetail.js
@@ -1,5 +1,34 @@
 window.addEventListener("DOMContentLoaded", function (events) 
 {
+	let body = document.getElementsByTagName("body")[0];
+
+	let loginBox = document.getElementById("login-register");
+	// show login and register box
+	let account = document.getElementById("account");
+	account.addEventListener("click", function(event)
+	{
+		var display = loginBox.style.display;
+		loginBox.style.display = 'block';
+	});
+	// close login and register box
+	let closeLogin = document.getElementById('close-login-box');
+	closeLogin.addEventListener("click", function(event)
+	{
+		loginBox.style.display = 'none';
+	});
+	// toggle mobile menu
+	let btnMenu = document.getElementById("btn-menu");
+	let mobileMenu = document.getElementById("mobile-menu");
+	btnMenu.addEventListener("click", function(event)
+	{
+		let isShow = mobileMenu.style.display;
+		if (isShow == "none") {
+			mobileMenu.style.display = "block";
+		} else{
+			mobileMenu.style.display = "none";
+		};
+		
+	});
 	let quantity = document.getElementById('quantity');
 	let btnSub = quantity.querySelector(".btn-sub");
 	let btnPlus = quantity.querySelector(".btn-plus");
@@ -19,4 +48,47 @@ window.addEventListener("DOMContentLoaded", function (events)
 		valueNum = valueNum + 1;
 		intNum.value = valueNum; 
 	});
+	// click images
+	let imagesProduct = document.getElementById("images-product");
+	let mainImage = imagesProduct.querySelector(".main-image");
+	let img = mainImage.querySelector('.img-product');
+	let ulImages = imagesProduct.querySelector("#list-images");
+	let listImg = ulImages.querySelectorAll(".small-image");
+	let listSrcImg = [];
+	for (var i = 0; i < listImg.length; i++) {
+		listSrcImg[i] = listImg[i].src;
+		
+	}
+	
+	listImg[0].addEventListener("click", function()
+	{
+		img.src = listImg[0].src;
+	});
+	listImg[1].addEventListener("click", function()
+	{
+		img.src = listImg[1].src;
+	});
+	listImg[2].addEventListener("click", function()
+	{
+		img.src = listImg[2].src;
+	});
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
